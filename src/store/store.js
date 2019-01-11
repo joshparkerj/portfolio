67fed41bbd2433a0f17a7abbd360fa785966559d
joshparkerj <superfakek@gmail.com>
2019-01-10 18:39:53 -0700

    get vuex going

---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import vue from 'vue';
+import vuex from 'vuex';
+
+vue.use(vuex);
+
+export const store = vuex.store({
+  state: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  }
+});
